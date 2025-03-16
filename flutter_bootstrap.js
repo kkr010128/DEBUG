261993ce9c0da940a4e63a66bebf13e57bfe6b54
kkr010128 <kkr010128@icloud.com>
2025-03-16 19:01:24 +0900

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "234137094"
+    serviceWorkerVersion: "2791340073"
   }
 });
